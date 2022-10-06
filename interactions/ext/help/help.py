b31d6dfbd51c1b88ef9bb56e1b9a463206be7969
ItsRqtl <itsrqtl@gmail.com>
2022-10-05 22:57:09 -0700

    feat: now it consider scope and permissions

--- interactions/ext/help/help.py
+++ interactions/ext/help/help.py
@@ -7,6 +7,7 @@ from interactions import (
     Button,
     Client,
     Command,
+    CommandContext,
     Embed,
     EmbedField,
     EmbedFooter,
@@ -16,6 +17,10 @@ from interactions import (
     extension_command,
 )
 
+try:
+    from interactions.api.models.attrs_utils import MISSING
+except ImportError:
+    from interactions.utils.attr_utils import MISSING
 
 class PaginatorFormat:
     def __init__(
@@ -45,6 +50,8 @@ class Help(Extension):
     def __init__(
         self,
         client,
+        consider_scope,
+        consider_permissions,
         embed_title,
         embed_description,
         embed_color,
@@ -59,6 +66,8 @@ class Help(Extension):
         no_category,
     ):
         self.client: Client = client
+        self.consider_scope: bool = consider_scope
+        self.consider_permissions: bool = consider_permissions  
         self.embed_title: str = embed_title
         self.embed_description: str = embed_description
         self.embed_color: int = embed_color
@@ -71,10 +80,6 @@ class Help(Extension):
         self.pagination: bool = pagination
         self.paginator_format: PaginatorFormat = paginator_format
         self.no_category: str = no_category
-
-        self.allCommands: list = []
-        self.embed: Embed = None
-        self.paginator: Paginator = None
 
     def parse_value(self, cmd: Command):
         value = ""
@@ -123,108 +128,104 @@ class Help(Extension):
 
     @extension_command(
         name="help",
-        description="Shows help message",
-        default_member_permissions=Permissions.DEFAULT,
+        description="Shows help message"
     )
-    async def _help(self, ctx):
-        if (
-            not self.allCommands
-            or self.allCommands != self.client._commands
-            or (self.embed is None and self.paginator is None)
-        ):
-            self.allCommands = self.client._commands
-            commands = {i.name: i for i in self.allCommands.copy()}
-            extensions = []
-            for i in self.client._extensions.values():
-                if isinstance(i, Extension) and not i.__module__.startswith("interactions.ext."):
-                    extensions.append(i)
-            extensions.sort(key=lambda x: x.__module__)
+    async def _help(self, ctx:CommandContext):
+        commands = {i.name: i for i in self.client._commands}
+        extensions = []
+        for i in self.client._extensions.values():
+            if isinstance(i, Extension) and not i.__module__.startswith("interactions.ext."):
+                extensions.append(i)
+        extensions.sort(key=lambda x: x.__module__)
 
-            fields = []
-            for ext in extensions:
-                if ext.__class__.__name__ in self.ignore_class:
-                    for command in ext._commands:
-                        commands.pop(command[8:])
-                else:
-                    value = ""
-                    for command in ext._commands:
-                        print(command[8:].lower())
-                        if command[8:].lower() not in self.ignore_command:
-                            cmd = commands[command[8:]]
+        fields = []
+        for ext in extensions:
+            if ext.__class__.__name__ in self.ignore_class:
+                for command in ext._commands:
+                    commands.pop(command[8:])
+            else:
+                value = ""
+                for command in ext._commands:
+                    if command[8:].lower() not in self.ignore_command:
+                        cmd = commands[command[8:]]
+                        if (not self.consider_scope or cmd.scope in [MISSING, None] or cmd.scope in [int(ctx.guild_id), ctx.guild]  or any(i in [int(ctx.guild_id), ctx.guild] for i in cmd.scope)) and (not self.consider_permissions or cmd.default_member_permissions in [MISSING, None] or (ctx.author.permissions and cmd.default_member_permissions in ctx.author.permissions)):
                             if self.subcommands:
                                 value += self.parse_value(cmd)
                             else:
                                 value += f"`{cmd.name}`{f' - {cmd.description}' if cmd.description != 'No description set' else ''}\n"
                         commands.pop(command[8:])
-                    if value:
-                        fields.append(
-                            EmbedField(name=ext.__class__.__name__, value=value, inline=False)
-                        )
-            if commands:
-                value = ""
-                for command in commands:
-                    if command.lower() not in self.ignore_command:
-                        cmd = commands[command]
+                if value:
+                    fields.append(
+                        EmbedField(name=ext.__class__.__name__, value=value, inline=False)
+                    )
+        if commands:
+            value = ""
+            for command in commands:
+                if command.lower() not in self.ignore_command:
+                    cmd = commands[command]
+                    if (not self.consider_scope or cmd.scope in [MISSING, None] or cmd.scope in [int(ctx.guild_id), ctx.guild]  or any(i in [int(ctx.guild_id), ctx.guild] for i in cmd.scope)) and (not self.consider_permissions or cmd.default_member_permissions in [MISSING, None] or (ctx.author.permissions and cmd.default_member_permissions in ctx.author.permissions)):
                         if self.subcommands:
                             value += self.parse_value(cmd)
                         else:
                             value += f"`{cmd.name}`{f' - {cmd.description}' if cmd.description != 'No description set' else ''}\n"
-                if value:
-                    fields.append(EmbedField(name=self.no_category, value=value, inline=False))
+            if value:
+                fields.append(EmbedField(name=self.no_category, value=value, inline=False))
 
-            if self.pagination and len(fields) > 1:
-                self.embed = None
-                self.paginator = Paginator(
-                    self.client,
-                    ctx,
-                    [
-                        Page(
-                            embeds=[
-                                Embed(
-                                    title=self.embed_title,
-                                    description=self.embed_description,
-                                    color=self.embed_color,
-                                    fields=[i],
-                                    footer=self.embed_footer,
-                                    timestamp=datetime.utcnow() if self.embed_timestamp else None,
-                                )
-                            ]
-                        )
-                        for i in fields
-                    ],
-                    (pf := self.paginator_format).timeout,
-                    pf.author_only,
-                    pf.use_buttons,
-                    False,
-                    pf.use_index,
-                    pf.extended_buttons,
-                    pf.buttons,
-                    pf.placeholder,
-                    pf.disable_after_timeout,
-                    pf.remove_after_timeout,
-                )
-            else:
-                self.paginator = None
-                self.embed = Embed(
-                    title=self.embed_title,
-                    description=self.embed_description,
-                    color=self.embed_color,
-                    fields=fields,
-                    footer=self.embed_footer,
-                    timestamp=datetime.utcnow() if self.embed_timestamp else None,
-                )
-        if self.embed:
+        if self.pagination and len(fields) > 1:
+            embed = None
+            paginator = Paginator(
+                self.client,
+                ctx,
+                [
+                    Page(
+                        embeds=[
+                            Embed(
+                                title=self.embed_title,
+                                description=self.embed_description,
+                                color=self.embed_color,
+                                fields=[i],
+                                footer=self.embed_footer,
+                                timestamp=datetime.utcnow() if self.embed_timestamp else None,
+                            )
+                        ]
+                    )
+                    for i in fields
+                ],
+                (pf := self.paginator_format).timeout,
+                pf.author_only,
+                pf.use_buttons,
+                False,
+                pf.use_index,
+                pf.extended_buttons,
+                pf.buttons,
+                pf.placeholder,
+                pf.disable_after_timeout,
+                pf.remove_after_timeout,
+            )
+        else:
+            paginator = None
+            embed = Embed(
+                title=self.embed_title,
+                description=self.embed_description,
+                color=self.embed_color,
+                fields=fields,
+                footer=self.embed_footer,
+                timestamp=datetime.utcnow() if self.embed_timestamp else None,
+            )
+        if embed:
             await ctx.send(
-                embeds=[self.embed],
+                embeds=[embed],
                 ephemeral=False if self.pagination else self.ephemeral,
             )
-        elif self.paginator:
-            self.paginator.ctx = ctx
-            await Paginator(**self.paginator._json).run()
+        elif paginator:
+            paginator.ctx = ctx
+            await Paginator(**paginator._json).run()
 
 
 def setup(
     client,
+    consider_scope=True, # only show commands that is available in guild
+    consider_permissions=True, # only show commands that the user can use
     embed_title: Optional[str] = "Help",  # Title of the embed
     embed_description: Optional[str] = "Here is a list of all commands",  # Description of the embed
     embed_color: Optional[int] = 0x000000,  # Color of the embed
@@ -244,6 +245,8 @@ def setup(
 ):
     return Help(
         client,
+        consider_scope,
+        consider_permissions,
         embed_title,
         embed_description,
         embed_color,
